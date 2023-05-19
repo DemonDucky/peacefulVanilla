3282186c0bb416a410be89bf8d3c557919267939
Luong Tuan anh <cuti1mat@gmail.com>
2023-05-19 10:42:27 +0700

    increase looks & performance

--- main.js
+++ main.js
@@ -18,16 +18,17 @@ async function writeData() {
 
         for (let recipe of shopkeeper.recipes) {
             const {shopName, shopOwner, location, world} = shopkeeper
+            const locationSplited = location.split(", ")
             const {resultItem, item1, item2, stock} = recipe
             result.push(`
     <tr>
         <td>${shopName}</td>
         <td>${shopOwner}</td>
-        <td>${location}</td>
+        <td><a href="https://peacefulvaanilla.club/mapping-test/#${world};flat;${locationSplited[0]},${locationSplited[1]},${locationSplited[2]};5" target="_blank">${location}</a></td>
         <td>${world}</td>
-        <td onmouseover="hello('${resultItem.type}')">${resultItem.type}</td>
-        <td>${item1.type}</td>
-        <td>${item2.type}</td>
+        <td onmouseover="hello('${resultItem.type}')">${resultItem.name || `${resultItem.amount}x ${resultItem.type.replaceAll("_", " ")}`}</td>
+        <td>${item1.name || `${item1.amount}x ${item1.type.replaceAll("_", " ")}`}</td>
+        <td>${item2 ? item2.name || `${item2.amount}x ${item2.type.replaceAll("_", " ")}` : ""}</td>
         <td>${stock}</td>
     </tr>`)
         }
