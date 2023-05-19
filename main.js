9ad9f67c9e13f5c268054e65b1a3e37026e202f5
Luong Tuan anh <cuti1mat@gmail.com>
2023-05-19 21:03:52 +0700

    added search feature

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-async function test() {
+const searchValue = document.querySelector("#search")
+
+
+async function getData() {
     const raw = await fetch("./data.json")
     const data = await raw.json()
     size = data.size
@@ -6,40 +9,60 @@ async function test() {
     return data
 }
 
-function hello(data) {
-    console.log(data)
-}
 async function writeData() {
 
-    const data = await test()
+    const data = await getData()
     const result = []
 
     for (let shopkeeper of data) {
-
-        for (let recipe of shopkeeper.recipes) {
-            const {shopName, shopOwner, location, world} = shopkeeper
-            const locationSplited = location.split(", ")
-            const {resultItem, item1, item2, stock} = recipe
-            result.push(`
-    <tr>
-        <td>${shopName}</td>
-        <td>${shopOwner}</td>
-        <td><a href="https://peacefulvaanilla.club/mapping-test/#${world};flat;${locationSplited[0]},${locationSplited[1]},${locationSplited[2]};5" target="_blank">${location}</a></td>
-        <td>${world}</td>
-        <td onmouseover="hello('${resultItem.type}')">${resultItem.name || `${resultItem.amount}x ${resultItem.type.replaceAll("_", " ")}`}</td>
-        <td>${item1.name || `${item1.amount}x ${item1.type.replaceAll("_", " ")}`}</td>
-        <td>${item2 ? item2.name || `${item2.amount}x ${item2.type.replaceAll("_", " ")}` : ""}</td>
-        <td>${stock}</td>
-    </tr>`)
-        }
+        result.push(convertData(shopkeeper))
     }
 
-    Clusterize({
+    const clusterize = Clusterize({
         rows: result,
         scrollId: 'scrollArea',
         contentId: 'contentArea'
     });
 
+
+    searchValue.addEventListener("input", () => {
+
+        const dataFiltered = filterData(data, searchValue.value)
+        const lastData = []
+        for (shopkeeper of dataFiltered) {
+            lastData.push(convertData(shopkeeper))
+        }
+        if (lastData.length === 0) lastData.push(`<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>`)
+        clusterize.update(lastData)
+    })
+
+}
+
+function filterData(data, filter) {
+
+    if (data === "") return data
+
+    return data.filter(shopkeeper => shopkeeper.shopName.toLowerCase().includes(filter.toLowerCase())
+    )
+}
+
+function convertData(shopkeeper) {
+    for (let recipe of shopkeeper.recipes) {
+        const {shopName, shopOwner, location, world} = shopkeeper
+        const locationSplited = location.split(", ")
+        const {resultItem, item1, item2, stock} = recipe
+        return (`
+    <tr>
+        <td>${shopName}</td>
+        <td>${shopOwner}</td>
+        <td><a href="https://peacefulvanilla.club/mapping-test/#${world};flat;${locationSplited[0]},${locationSplited[1]},${locationSplited[2]};5" target="_blank">${location}</a></td>
+        <td>${world}</td>
+        <td>${resultItem.name || `${resultItem.amount}x ${resultItem.type.replaceAll("_", " ")}`}</td>
+        <td>${item1.name || `${item1.amount}x ${item1.type.replaceAll("_", " ")}`}</td>
+        <td>${item2 ? item2.name || `${item2.amount}x ${item2.type.replaceAll("_", " ")}` : ""}</td>
+        <td>${stock}</td>
+    </tr>`)
+    }
 }
 
 writeData()
